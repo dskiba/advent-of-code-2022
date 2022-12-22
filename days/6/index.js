6ae83e3ba45591c37929023eb04111367337f176
Dan Skiba <skdenx@gmail.com>
2022-12-22 22:57:34 +0700

    add day 6

---
+++ days/6/index.js
@@ -0,0 +1,35 @@
+const fs = require('fs/promises');
+
+
+function calculate(str, window) {
+  let start = 0
+  let end = window
+  let res = null
+  while (!res && end < str.length) {
+    const sliced = str.substring(start,end)
+    if(new Set([...sliced]).size===window) {
+      res = end
+    }
+    start++
+    end++
+  }
+  return res
+}
+
+
+async function Five_pOne() {
+  const data = await fs.readFile('./data.txt', { encoding: 'utf8' });
+  console.log(calculate(data.trimEnd(), 4))
+}
+
+
+// Five_pOne(); // 1134
+
+async function Five_pTwo() {
+  const data = await fs.readFile('./data.txt', { encoding: 'utf8' });
+  console.log(calculate(data.trimEnd(), 14))
+
+}
+
+
+Five_pTwo(); // 2263
